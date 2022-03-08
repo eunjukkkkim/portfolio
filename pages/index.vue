8490fb3d2d397a6f05a1f099fc925e5e0140c4f1
eunju <kejgood77@gmail.com>
2022-03-08 13:23:09 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- navBar -->
+    <nav id="navbar">
+      <div class="navbar__logo">
+        <a href=""> #EunJu,Kim</a>
+      </div>
+      <div class="navbar__menu">
+        <ul class="navbar__menu">
+          <li class="navbar__menu__item">Home</li>
+          <li class="navbar__menu__item">About</li>
+          <li class="navbar__menu__item">Skills</li>
+          <li class="navbar__menu__item">Works</li>
+          <li class="navbar__menu__item">Contact</li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Home -->
+    <section id="home">
+      <h1>EunJu, Kim</h1>
+      <img src="../assets/profile.png" alt="" width="300px" height="300px" />
+      <h2>Frontend Developer</h2>
+      <button>contact me</button>
+    </section>
+
+    <!-- About -->
+    <section>
+      <h1>About Me</h1>
+    </section>
+    <section>
+      <h1>Skills</h1>
+    </section>
+    <section>
+      <h1>Works</h1>
+    </section>
+    <section>
+      <h1>Contact Me</h1>
+    </section>
+  </div>
+  <!-- <Tutorial/> -->
+</template>
+
+<script>
+export default {};
+</script>
